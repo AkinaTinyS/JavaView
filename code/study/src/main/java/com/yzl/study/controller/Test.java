e644d36e928036ca6352ce829b780cd81124fcb2
游泽灵 <youzl@xmpaoyou.com>
2021-02-04 17:55:06 +0800

    更新集合，部分数据结构

--- code/study/src/main/java/com/yzl/study/controller/Test.java
+++ code/study/src/main/java/com/yzl/study/controller/Test.java
@@ -1,8 +1,16 @@
 package com.yzl.study.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
+
+        List<String> b = new ArrayList();
+
+        List a = Collections.singletonList(b);
 
     }
 }
