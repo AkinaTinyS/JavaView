6d861706305dc8930450d7c3b83b625f9c25f07d
游泽灵 <youzl@xmpaoyou.com>
2021-05-26 16:15:28 +0800

    one end

--- code/study/src/main/java/com/yzl/study/controller/Test.java
+++ code/study/src/main/java/com/yzl/study/controller/Test.java
@@ -1,16 +1,26 @@
 package com.yzl.study.controller;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        List<String> b = new ArrayList();
+//        int a=Integer.valueOf(args[0]);
+//        int b=Integer.valueOf(args[1]);
+        Scanner input=new Scanner(System.in);
+        String str=input.toString();
+        int n=input.nextInt();
+        System.out.println(n);
 
-        List a = Collections.singletonList(b);
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        File file = new File("a.txt");
 
+        System.out.println(file.getAbsolutePath());
     }
 }
